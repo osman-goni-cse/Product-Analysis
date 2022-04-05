31ee451778db41e9f990310b73633de97d688a50
osman-goni-cse <osmancse32@gmail.com>
2022-04-05 08:55:35 +0600

    line chart and area chart added using recharts

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  BarChart,
+  Bar,
+  Legend,
+  Label,
+} from "recharts";
+import React from "react";
 
 const Dashboard = () => {
+  const data = [
+    {
+      month: "Mar",
+      investment: 100000,
+      sell: 241,
+      revenue: 10401,
+    },
+    {
+      month: "Apr",
+      investment: 200000,
+      sell: 423,
+      revenue: 24500,
+    },
+    {
+      month: "May",
+      investment: 500000,
+      sell: 726,
+      revenue: 67010,
+    },
+    {
+      month: "Jun",
+      investment: 500000,
+      sell: 529,
+      revenue: 40405,
+    },
+    {
+      month: "Jul",
+      investment: 600000,
+      sell: 601,
+      revenue: 50900,
+    },
+    {
+      month: "Aug",
+      investment: 700000,
+      sell: 670,
+      revenue: 61000,
+    },
+  ];
   return (
-    <div>
-      <h1>Dashboard Dashboard</h1>
+    <div className="mt-5 d-flex">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="month" />
+        <YAxis dataKey="sell" />
+        <Tooltip />
+        
+      </LineChart>
+
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+        <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+      </BarChart>
     </div>
   );
 };
